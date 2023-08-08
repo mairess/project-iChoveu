5ca8401afd82177cadb9220c12adf0b1c8d4d624
Maires Souza <trabalho.maires@gmail.com>
2023-08-08 11:00:26 -0300

    refactor: token import

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,16 +1,17 @@
 // Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
 
 export const searchCities = async (TERMO_DE_BUSCA) => {
   const erMessage = 'Nenhuma cidade encontrada';
   try {
-    const TOKEN = 'd50ff4be4f7f485ea18235637230708';
     const API_URL = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${TERMO_DE_BUSCA}`;
     const response = await fetch(API_URL);
     const data = await response.json();
     if (!TERMO_DE_BUSCA || data.length === 0) {
       throw new Error(erMessage);
     }
-    return data;
+    const [city] = data;
+    return city;
   } catch (error) {
     return window.alert(error.message);
   }
@@ -21,7 +22,6 @@ export const getWeatherByCity = async (URL_CIDADE) => {
     if (!URL_CIDADE) {
       throw new Error(`Requesting error: ${erMessage}`);
     }
-    const TOKEN = 'd50ff4be4f7f485ea18235637230708';
     const API_CITY_URL = `http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${URL_CIDADE}`;
     const response = await fetch(API_CITY_URL);
     const data = await response.json();
