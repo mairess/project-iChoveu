fd3faaf3731e9ea482ebf5cd666451195835d045
Maires Souza <trabalho.maires@gmail.com>
2023-08-08 10:07:01 -0300

    feat: function getWeatherByCity implemented

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -16,6 +16,20 @@ export const searchCities = async (TERMO_DE_BUSCA) => {
   }
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+export const getWeatherByCity = async (URL_CIDADE) => {
+  try {
+    if (!URL_CIDADE) {
+      throw new Error(`Requesting error: ${erMessage}`);
+    }
+    const TOKEN = 'd50ff4be4f7f485ea18235637230708';
+    const API_CITY_URL = `http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${URL_CIDADE}`;
+    const response = await fetch(API_CITY_URL);
+    const data = await response.json();
+    const { current } = data;
+    const { temp_c: temp, condition: cityCondition } = current;
+    const { text: condition, icon } = cityCondition;
+    return { temp, condition, icon };
+  } catch (error) {
+    return window.alert(error.message);
+  }
 };
