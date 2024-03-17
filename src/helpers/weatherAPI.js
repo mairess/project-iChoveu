76d6417a391b90bffa66c5611544076fde0d0292
mairess <trabalho.maires@gmail.com>
2024-03-17 08:36:32 -0300

    refactor: add https protocolo

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -4,7 +4,7 @@ const TOKEN = import.meta.env.VITE_TOKEN;
 export const searchCities = async (TERMO_DE_BUSCA) => {
   const erMessage = 'Nenhuma cidade encontrada';
   try {
-    const API_URL = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${TERMO_DE_BUSCA}`;
+    const API_URL = `https://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${TERMO_DE_BUSCA}`;
     const response = await fetch(API_URL);
     const data = await response.json();
     if (!TERMO_DE_BUSCA || data.length === 0) {
@@ -21,7 +21,7 @@ export const getWeatherByCity = async (url) => {
     if (!url) {
       throw new Error(`Requesting error: ${erMessage}`);
     }
-    const API_CITY_URL = `http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${url}`;
+    const API_CITY_URL = `https://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${url}`;
     const response = await fetch(API_CITY_URL);
     const data = await response.json();
     const { location, current } = data;
@@ -36,7 +36,7 @@ export const getWeatherByCity = async (url) => {
 
 export const getNextSevenDaysForecast = async (url) => {
   const DIAS = 7;
-  const API_NEXT_DAYS_FORECAST = `http://api.weatherapi.com/v1/forecast.json?lang=pt&key=${TOKEN}&q=${url}&days=${DIAS}`;
+  const API_NEXT_DAYS_FORECAST = `https://api.weatherapi.com/v1/forecast.json?lang=pt&key=${TOKEN}&q=${url}&days=${DIAS}`;
 
   const response = await fetch(API_NEXT_DAYS_FORECAST);
   const data = await response.json();
